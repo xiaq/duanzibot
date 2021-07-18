e1b5cdf1a7725058486a9153e1bf8e15c795bd4e
Qi Xiao <xiaqqaix@gmail.com>
2021-07-18 21:02:08 +0100

    Simplify, and support getting random duanzi

--- duanzibot.go
+++ duanzibot.go
@@ -4,52 +4,37 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
-	"net/url"
+	"strconv"
 	"strings"
+	"sync"
+	"time"
 
 	"github.com/xiaq/tg"
 )
 
-type DuanziBot struct {
+func main() {
+	buf, err := ioutil.ReadFile("token.txt")
+	if err != nil {
+		log.Fatalf("cannot read token file: %s", err)
+	}
+	token := strings.TrimSpace(string(buf))
+
+	newBot(token).Main()
+}
+
+type bot struct {
 	*tg.CommandBot
 }
 
-type Reply struct {
-	Body string `json:"body"`
+func newBot(token string) *bot {
+	b := &bot{tg.NewCommandBot(token)}
+	b.OnCommand("duanzi", b.handleDuanzi)
+	return b
 }
 
-var errormsg = "发生了错误哦。快 /pia 我的主人吧。"
-
-func get(text string) string {
-	/*
-		_, err := strconv.Atoi(text)
-		if err != nil {
-			return "要给我数字哦。"
-		}
-	*/
-	url := "https://api.github.com/repos/tuna/collection/issues/" + url.QueryEscape(text)
-	resp, err := http.Get(url)
-	if err != nil {
-		log.Printf("error http.Get(%q): %v\n", url, err)
-		return errormsg
-	}
-	defer resp.Body.Close()
-
-	var reply Reply
-	err = json.NewDecoder(resp.Body).Decode(&reply)
-	if err != nil {
-		log.Printf("error decoding: %v\n", err)
-		return "发生了错误哦。快 /pia 我的主人吧。"
-	}
-	if reply.Body == "" {
-		return "GET 不到这个段子哦。"
-	}
-	return "https://github.com/tuna/collection/issues/" + text
-	//return reply.Body
-}
-
-func (b *DuanziBot) handleDuanzi(_ *tg.CommandBot, text string, msg *tg.Message) {
+func (b *bot) handleDuanzi(_ *tg.CommandBot, text string, msg *tg.Message) {
 	log.Println("/duanzi", text)
 	err := b.Get("/sendMessage", tg.Query{
 		"chat_id": msg.Chat.ID,
@@ -60,18 +45,54 @@ func (b *DuanziBot) handleDuanzi(_ *tg.CommandBot, text string, msg *tg.Message)
 	}
 }
 
-func NewDuanziBot(token string) *DuanziBot {
-	b := &DuanziBot{tg.NewCommandBot(token)}
-	b.OnCommand("duanzi", b.handleDuanzi)
-	return b
+func get(text string) string {
+	text = strings.TrimSpace(text)
+	if text == "" {
+		// Find a random duanzi
+		n := getMaxIssueNumber()
+		i := rand.Intn(n) + 1
+		return "https://github.com/tuna/collection/issues/" + strconv.Itoa(i)
+	}
+	if i, err := strconv.Atoi(text); err == nil {
+		return "https://github.com/tuna/collection/issues/" + strconv.Itoa(i)
+	}
+	return "用法：/duanzi [id] （省略 id 则随机选择段子）"
 }
 
-func main() {
-	buf, err := ioutil.ReadFile("token.txt")
-	if err != nil {
-		log.Fatalf("cannot read token file: %s", err)
-	}
-	token := strings.TrimSpace(string(buf))
+type issuesReply []struct{ Number int }
 
-	NewDuanziBot(token).Main()
+var maxIssueNumber struct {
+	sync.Mutex
+	value  int
+	update time.Time
+}
+
+const cacheTTL = 30 * time.Minute
+
+func getMaxIssueNumber() int {
+	maxIssueNumber.Lock()
+	defer maxIssueNumber.Unlock()
+
+	if time.Now().Sub(maxIssueNumber.update) > cacheTTL {
+		url := "https://api.github.com/repos/tuna/collection/issues?per_page=1"
+		resp, err := http.Get(url)
+		if err != nil {
+			log.Println("list issues:", err)
+			return 1
+		}
+		var reply issuesReply
+		err = json.NewDecoder(resp.Body).Decode(&reply)
+		if err != nil {
+			log.Println("decode issue list:", err)
+			return 1
+		}
+		if len(reply) != 1 {
+			log.Println("length of issue list is", len(reply))
+			return 1
+		}
+
+		maxIssueNumber.value = reply[0].Number
+		maxIssueNumber.update = time.Now()
+	}
+	return maxIssueNumber.value
 }
